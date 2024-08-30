030cc6d074a50426d6d6d0e19c8cb0f3052e4517
MrRayliegh <edwardcole143@gmail.com>
2024-08-30 23:49:21 +0800

    Create sbac.lua

---
+++ sbac.lua
@@ -0,0 +1,11 @@
+return {
+    487237, -- OWNER
+    29940,   -- Anakecil
+    469440,  -- quandingle
+    447519,  -- CHOX
+    568461,  -- Zeess
+    52177,   -- papachulo
+    554244,  -- Dispo
+    139580,  -- apacoba
+    61208    -- SFA7
+}
